ecda903ee37604375bac7f3a2bf61974107d7c88
dario <kienkane@MacBook-Pro-de-dario.local>
2019-04-03 09:50:30 -0600

    random lorem text, home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,12 @@
+
 <template lang="pug">
   .pruebaContenedor.w-100.center.tc
     h1 {{ msg }}
     p {{ descripcion }}
     #gridSpringboard.vh-100.center.pa2-l.pa1 
-      div 
-        | Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        
-      div 
-        | Maecenas feugiat luctus enim a porttitor. Vivamus dapibus
-        
-      div 
-        | Aenean porta venenatis consectetur. Class aptent taciti.
-        
-      div 
-        | Nullam ac condimentum dui, porttitor egestas turpis.
-        
-      div
-        | Fusce facilisis, odio nec viverra tempus, eros arcu blandit nisi.
-        
-      div 
-        | Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        
-      div 
-        | Maecenas feugiat luctus enim a porttitor. Vivamus dapibus
-        
-      div 
-        | Aenean porta venenatis consectetur. Class aptent taciti.
-        
-      div 
-        | Nullam ac condimentum dui, porttitor egestas turpis.
-        
-      div
-        | Fusce facilisis, odio nec viverra tempus, eros arcu blandit nisi.
-        
-      div 
-        | Nullam ac condimentum dui, porttitor egestas turpis.
-        
-      div
-        | Fusce facilisis, odio nec viverra tempus, eros arcu blandit nisi.
+      div.cosa(v-for="index in lmax")
+        | {{ index }}
+        | {{ loremArray[aleatorio(min, max)].msg}}
     
 </template>
 
@@ -46,13 +15,27 @@ export default {
   name: 'home',
   data () {
     return {
-      min: 0,
-      max:360,
+      min: 1,
+      max: 3,
+      lmax:12,
       msg: 'Patrón Springboard',
-      descripcion: 'El patrón Springboard, también llamado Launchpad, fue el patrón de navegación más popular en 2011. Este diseño es una pantalla de inicio con opciones que actúan como puntos de inicio en la aplicación.'
+      descripcion: 'El patrón Springboard, también llamado Launchpad, fue el patrón de navegación más popular en 2011. Este diseño es una pantalla de inicio con opciones que actúan como puntos de inicio en la aplicación.',
+      loremArray: [
+        {msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+        {msg: 'Maecenas feugiat luctus enim a porttitor. Vivamus dapibus'},
+        {msg: 'Aenean porta venenatis consectetur. Class aptent taciti'},
+        {msg: 'Nullam ac condimentum dui, porttitor egestas turpis'},
+        {msg:'Fusce facilisis, odio nec viverra tempus, eros arcu blandit nisi'}
+      ]
+    }
+  },
+  methods:{
+    aleatorio: function(a,b) {
+      return Math.round(Math.random()*(b-a)+parseInt(a));
     }
   }
 }
+
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style  lang="scss">
@@ -72,7 +55,7 @@ export default {
       display:grid;
       width:98%;
       grid-template-columns: 24% 24% 24% 24%;
-      grid-template-rows: 33% 33% 33%;
+      grid-template-rows: 32% 32% 32%;
       grid-gap: 1.3%;
       grid-row-gap: 2%;
       border:1px solid $blue;
